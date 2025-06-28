b5dceb6fe1801a98ad248e724a3497de04e055dd
Sandip Tamang <136832190+SandipTamangDev@users.noreply.github.com>
2025-06-28 08:22:54 +0545

    (#8)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio-builder',
+  base: '/my-portfolio-builder/',
   plugins: [react()],
 })
+
