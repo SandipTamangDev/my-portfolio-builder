6c384d333c8c74200b7b7d99e4997be4c74a43cb
Sandip Tamang <136832190+SandipTamangDev@users.noreply.github.com>
2025-06-29 10:42:18 +0545

    (#11)

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/my-portfolio-builder/">  {/* <-- Set your base path */}
+    <BrowserRouter basename="/">  {/* <-- Set your base path */}
       <App />
     </BrowserRouter>
   </StrictMode>,
